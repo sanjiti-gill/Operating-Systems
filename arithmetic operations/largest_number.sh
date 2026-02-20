84b639953c54253acf5a23b7856f054335214e79
Sanjiti <sanjitishergill@gmail.com>
2026-02-20 21:44:48 +0530

    Create largest_number.sh

---
+++ arithmetic operations/largest_number.sh
@@ -0,0 +1,16 @@
+echo "Largest of 3 numbers"
+echo "Enter 3 numbers:"
+
+read a
+read b
+read c
+
+if [ $a -gt $b ] && [ $a -gt $c ]
+then
+    echo "$a is the largest"
+elif [ $b -gt $a ] && [ $b -gt $c ]
+then
+    echo "$b is the largest"
+else
+    echo "$c is the largest"
+fi
